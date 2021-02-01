2a9e0020ae220555feadf66c5f02df3afc5aa580
harisgaul7 <ckgb02@gmail.com>
2021-02-01 10:23:57 +0700

    Unchecked checkbox action

--- app/src/main/java/haris/org/futureschool/JurusanActivity.java
+++ app/src/main/java/haris/org/futureschool/JurusanActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,29 +16,36 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class JurusanActivity extends AppCompatActivity {
 
-    String isi;
+    ArrayList<String> cekbox;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_jurusan);
 
-        LinearLayout tampung = findViewById(R.id.ll_tampung_jurusan);
-        isi = "";
-        for(int i = 0; i < 5; i++) {
-            final CheckBox ch = new CheckBox(getApplicationContext());
-            ch.setText("Aku angka "+i);
-            ch.setId(i);
-            tampung.addView(ch);
+        final LinearLayout tampung = findViewById(R.id.ll_tampung_jurusan);
+        cekbox = new ArrayList<>();
 
-            ch.setOnClickListener(new View.OnClickListener() {
+        for(int i = 0; i < 5; i++) {
+            final CheckBox gg = new CheckBox(getApplicationContext());
+            gg.setText("Aku angka "+i);
+            gg.setId(i);
+            tampung.addView(gg);
+
+            gg.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (ch.isChecked()){
-                        isi+=ch.getText();
+                    if (gg.isChecked()){
+                        cekbox.add(gg.getText().toString());
+                    }
+                    else if (!gg.isChecked()){
+                        cekbox.remove(gg.getText().toString());
                     }
                 }
             });
@@ -47,6 +55,10 @@ public class JurusanActivity extends AppCompatActivity {
         hasil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String isi = "";
+                for (int i = 0; i < cekbox.size(); i++) {
+                    isi+=cekbox.get(i);
+                }
                 Toast.makeText(JurusanActivity.this, "Hasil = "+isi, Toast.LENGTH_SHORT).show();
             }
         });
